dd8376d6708c49cdeb8302674b4d73e6fb3c8cbd
adviislam10 <71040123+adviislam10@users.noreply.github.com>
2020-09-23 13:27:08 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -10,24 +10,25 @@ document.getElementById('allNick').addEventListener('click', allNick);
 
 // Random Nickname
 function randomNick() {
+    document.getElementById('output0').innerHTML = ''
+
     let firstName = document.getElementById('firstName').value;
     let lastName = document.getElementById('lastName').value;
     imgIndex = Math.randomInt(0, nickArray.length);
-    document.getElementById('output0').innerHTML = firstName + ' ' + '"' + nickArray[imgIndex] + '"' + ' ' + lastName
+    document.getElementById('output0').innerHTML += firstName + ' ' + '"' + nickArray[imgIndex] + '"' + ' ' + lastName + '<br>'
 
 }
 
 // All Nicknames
 
 function allNick() {
+    document.getElementById('output0').innerHTML = ''
+
     let firstName = document.getElementById('firstName').value;
     let lastName = document.getElementById('lastName').value;
-    document.getElementById('output0').innerHTML = firstName + ' ' + '"' + nickArray[0] + '"' + ' ' + lastName
-    document.getElementById('output1').innerHTML = firstName + ' ' + '"' + nickArray[1] + '"' + ' ' + lastName
-    document.getElementById('output2').innerHTML = firstName + ' ' + '"' + nickArray[2] + '"' + ' ' + lastName
-    document.getElementById('output3').innerHTML = firstName + ' ' + '"' + nickArray[3] + '"' + ' ' + lastName
-    document.getElementById('output4').innerHTML = firstName + ' ' + '"' + nickArray[4] + '"' + ' ' + lastName
-    document.getElementById('output5').innerHTML = firstName + ' ' + '"' + nickArray[5] + '"' + ' ' + lastName
-    document.getElementById('output6').innerHTML = firstName + ' ' + '"' + nickArray[6] + '"' + ' ' + lastName
+
+    for (let i = 0; i < nickArray.length; i++)
+        document.getElementById('output0').innerHTML += firstName + ' ' + '"' + nickArray[i] + '"' + ' ' + lastName + '<br>'
+
 
 }
